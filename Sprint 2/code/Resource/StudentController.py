9459cad409a650caa5dfd0fd7bec2ae89dbea990
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2023-07-23 23:36:44 -0400

    Student profile submission and saving and showing on ui

--- Sprint 2/code/Resource/StudentController.py
+++ Sprint 2/code/Resource/StudentController.py
@@ -5,9 +5,10 @@ from flask import url_for
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username', type=str, required=False, help='Add the username into the system')
 _user_parser.add_argument('highestQualification', type=str, required=False, help='Add the highestQualification into the system')
-_user_parser.add_argument('work_experience', type=int, required=False, help='Add the work_experience into the system')
+_user_parser.add_argument('work_experience', type=str, required=False, help='Add the work_experience into the system')
 _user_parser.add_argument('achivements', type=str, required=False, help='Add the achivements into the system')
 _user_parser.add_argument('email', type=str, required=False, help='Add the email into the system')
+_user_parser.add_argument('gender', type=str, required=False, help='Add the gender into the system')
 _user_parser.add_argument('age', type=str, required=False, help='Add the age into the system')
 _user_parser.add_argument('address', type=str, required=False, help='Add the address into the system')
 _user_parser.add_argument('phone', type=str, required=False, help='Add the phone number into the system')
@@ -17,6 +18,6 @@ class CrStudent(Resource):
         data = _user_parser.parse_args()
         student = Student(**data)
         student.save_to_db()
-        return "student profile created"
+        return {'studentId': student.id, 'msg': 'student created success'}
 
 
